a3a7514aa638a3a250b1c8e8f3a35df1cbce8805
Nuttawat <nuttawat.ntck@gmail.com>
2026-08-08 15:39:23 +0700

    Update PLAYOS-Update.sh

--- PLAYOS-Update.sh
+++ PLAYOS-Update.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 LOG_FILE="/home/ark/playos-update.log"
 
-echo ">> เริ่มกระบวนการอัปเดต PLAY OS ผ่านระบบ OTA..." | tee -a "$LOG_FILE"
+echo ">> START UPDEATE PLAY OS WITH OTA..." | tee -a "$LOG_FILE"
 
 
 
 if [ -x "$(command -v msgbox)" ]; then
-    sudo msgbox "PLAY OS OTA Update\n\nการเชื่อมต่อระบบอัปเดตเสร็จสมบูรณ์แล้ว!"
+    sudo msgbox "PLAY OS OTA Update\n\n Finish!"
 fi
 
 
