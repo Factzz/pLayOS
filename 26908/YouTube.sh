e916aa2b0e30430b0b2c51325994674b4c4740c5
Nuttawat <nuttawat.ntck@gmail.com>
2026-08-09 21:53:08 +0700

    Update YouTube.sh

--- 26908/YouTube.sh
+++ 26908/YouTube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ชี้เป้าไปที่โฟลเดอร์ไส้ในของแอป (ที่ซ่อนไว้เพื่อไม่ให้เมนู ES รก)
-cd /roms/ports/Tubelite/
+cd /roms/ports/Tubelite/Tubelite/
 
 # สั่งรันสคริปต์ระบบหลัก
 bash ./TubeLite.tbl
